d48b5e858bcf26c9d794ac11b74e65292d81f963
dianasou <dsouthiphomma@gmail.com>
2021-10-15 16:24:55 -0500

    Update CSS and JS

--- js/script.js
+++ js/script.js
@@ -37,9 +37,10 @@ function decrease() {
 
 // manual slideshow
 var slideIndex = 1;
+
 showSlides(slideIndex);
 
-// next/previous controls
+// Next/previous controls
 function plusSlides(n) {
   showSlides(slideIndex += n);
 }
@@ -51,34 +52,35 @@ function currentSlide(n) {
 
 function showSlides(n) {
   var i;
-  var slides = document.getElementsByClassName("mySlides");
-  var dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
+  var slides = document.getElementsByClassName('mySlides');
+  if (n > slides.length) {
+    slideIndex = 1;
+  }
+  if (n < 1) {
+    slideIndex = slides.length;
+  }
   for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
+    slides[i].style.display = 'none';
   }
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex - 1].style.display = "block";
-  dots[slideIndex-1].className += " active";
+  slides[slideIndex - 1].style.display = 'block';
 }
 
 /*
 // automatic slideshow
-var slideIndex = 0;
-showSlides();
+var slideIndex2 = 0;
+carousel();
 
-function showSlides() {
-  var i;
-  var slides = document.getElementsByClassName("mySlides2");
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
+function carousel() {
+  var z;
+  var slides2 = document.getElementsByClassName('mySlides2');
+  for (z = 0; z < slides2.length; z++) {
+    slides2[z].style.display = 'none';
   }
-  slideIndex++;
-  if (slideIndex > slides.length) {slideIndex = 1}
-  slides[slideIndex-1].style.display = "block";
-  setTimeout(showSlides, 4000); // Change image every 4 seconds
+  slideIndex2++;
+  if (slideIndex2 > slides2.length) {
+    slideIndex2 = 1;
+  }
+  slides2[slideIndex2 - 1].style.display = 'block';
+  setTimeout(carousel, 4000); // Change image every 4 seconds
 }
 */
